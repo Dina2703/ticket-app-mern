f45f273fd345626b8da484a8428a985e76d93920
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-06-10 13:23:15 -0400

    Error handling and install express-async-handler package

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+//middlewares to get data from the body (req.body) otherwise we.ll get 'underfined'
+app.use(express.json()); //it allows us to send json
+app.use(express.urlencoded({ extended: false })); //to accept the form of the URL encoded form
 
 //create a route with Express
 app.get("/", (req, res) => {
@@ -11,5 +16,7 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
