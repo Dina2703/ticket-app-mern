73534aa261235c106832be891a79e7074af88599
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-06-10 18:09:53 -0400

    Protect Routes & Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,6 +72,19 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc Get current User
+//@route /api/users/me
+//@access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
+
+//Generate token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "60d",
@@ -81,4 +94,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
