3c194e5e0fc6bb39aa9867989ca421a644de3caf
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-06-11 17:11:31 -0400

    Home, Register, Login UI

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,63 @@
-import React from "react";
+import { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 function Login() {
-  return <div>Login</div>;
+  const [formDate, setFormDate] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formDate;
+
+  const onChange = (e) => {
+    setFormDate((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please login to get support</p>
+      </section>
+      <section>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="email"
+              id="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+              required
+            />
+            <input
+              className="form-control"
+              type="password"
+              id="password"
+              value={password}
+              placeholder="Enter password"
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Login;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,7 +1,87 @@
-import React from "react";
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 function Register() {
-  return <div>Register</div>;
+  const [formDate, setFormDate] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formDate;
+
+  const onChange = (e) => {
+    setFormDate((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      toast.error("passwords do not match");
+    }
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              id="name"
+              value={name}
+              placeholder="Enter your name"
+              onChange={onChange}
+              required
+            />
+            <input
+              className="form-control"
+              type="email"
+              id="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+              required
+            />
+            <input
+              className="form-control"
+              type="password"
+              id="password"
+              value={password}
+              placeholder="Enter password"
+              onChange={onChange}
+              required
+            />
+            <input
+              className="form-control"
+              type="password"
+              id="password2"
+              value={password2}
+              placeholder="Confirm password"
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Register;
