ab1d0e9d5307243f4757848529369e8d9e1190df
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-06-13 22:21:58 -0400

    Single Ticket, Update & Delete methods on it

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -20,6 +20,34 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+//@desc Get user tickets
+//@route GET /api/tickets/:id
+//@access Private
+const getTicket = asyncHandler(async (req, res) => {
+  //Get user using the ID in the JWT(token)
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  //if the ticket was not created by the user. 'req.user.id' is logged in user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  res.status(200).json(ticket);
+});
+
 //@desc Create new ticket
 //@route POST /api/tickets
 //@access Private
@@ -50,7 +78,73 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
+//@desc Delete user tickets
+//@route DELETE /api/tickets
+//@access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  //Get user using the ID in the JWT(token)
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+//@desc Update tickets
+//@route PUT /api/tickets/:id
+//@access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  //Get user using the ID in the JWT(token)
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  //if the ticket was not created by the user. 'req.user.id' is logged in user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
 module.exports = {
   getTickets,
   createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
 };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { getTickets, createTicket } = require("../controllers/ticketController");
+const {
+  getTickets,
+  getTicket,
+  createTicket,
+  deleteTicket,
+  updateTicket,
+} = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/AuthMiddleware");
 
+//router.route("/") - route() allows us chain methods like .post().get().delete() to the same route
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+//route for single ticket
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
